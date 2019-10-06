2f2e4911a9a98e4a4557adfc160eb4b2fc7ff6fe
namjals615@gmail.com <namjals@naver.com>
2019-10-07 05:29:47 +0900

    주석 변경

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from model import LinearRegression
 from optimizer import SGD
 from functions import least_error_square
 
-#  방법 1 LinearRegression 사용
+#  방법 1 Gradient Decent 사용
 data = read_data('../data/count_by_date.csv')
 x, t = list(map(lambda x: [int(x)], data['INDEX'])), list(map(lambda x: [int(x)], data['CNT']))
 x, t = np.array(x), np.array(t)
@@ -17,7 +17,7 @@ for e in range(epoch):
     model.backward()
     optimizer.update(model.params, model.grads)
     m, b = model.get_params() # 기울기, 편향
-    # print(loss, m[0][0], b[0][0]) #
+    # print(loss, m[0][0], b[0][0])
 
 print(loss, m[0][0], b[0][0])
 print(model.predict(np.array([[i] for i in range(31, 61)]))) # INDEX가 31부터 60까지 추론
